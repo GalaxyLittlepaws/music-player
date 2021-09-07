d09e849d929939991eb57f218ab4eeae076c52f0
Kat Galaxy <elismithtech@gmail.com>
2021-09-06 21:52:57 -0400

    First Working Build
    This seems to work as expected

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # music-player
  A music player built with vanilla HTML, CSS, and Javascript. To host on IPFS.
+
+ See original tutorial code: https://www.geeksforgeeks.org/create-a-music-player-using-javascript/
+ Some of the CSS code and original idea from: https://dev.to/kunaal438/how-to-create-music-player-with-pure-html-css-js-c1j
---
+++ data.js
@@ -0,0 +1,62 @@
+let track_list = [
+    {
+        name: 'Dark Journey',
+        path: 'music/01.mp3',
+        artist: 'Wayne Kinos',
+        image: 'images/cover.jpg'
+    },
+    {
+        name: 'A New World',
+        path: 'music/02.mp3',
+        artist: 'Wayne Kinos',
+        image: 'images/cover.jpg'
+    },
+    {
+        name: 'Exploring',
+        path: 'music/03.mp3',
+        artist: 'Wayne Kinos',
+        image: 'images/cover.jpg'
+    },
+    {
+        name: 'Settling In',
+        path: 'music/04.mp3',
+        artist: 'Wayne Kinos',
+        image: 'images/cover.jpg'
+    },
+    {
+        name: 'To The Vet',
+        path: 'music/05.mp3',
+        artist: 'Wayne Kinos',
+        image: 'images/cover.jpg'
+    },
+    {
+        name: 'In A Catnip Garden',
+        path: 'music/06.mp3',
+        artist: 'Wayne Kinos',
+        image: 'images/cover.jpg'
+    },
+    {
+        name: 'The Zoomies',
+        path: 'music/07.mp3',
+        artist: 'Wayne Kinos',
+        image: 'images/cover.jpg'
+    },
+    {
+        name: 'Cat Nap',
+        path: 'music/08.mp3',
+        artist: 'Wayne Kinos',
+        image: 'images/cover.jpg'
+    },
+    {
+        name: 'Snooping Around',
+        path: 'music/09.mp3',
+        artist: 'Wayne Kinos',
+        image: 'images/cover.jpg'
+    },
+    {
+        name: 'Cat Dreams',
+        path: 'music/10.mp3',
+        artist: 'Wayne Kinos',
+        image: 'images/cover.jpg'
+    },
+]
